d0cb7d3d4688eb84e29e653e473262e2b615ef3b
WelingtonMoraes <welingtonfernando282@gmail.com>
2022-01-02 02:28:35 -0300

    MDS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import uservatar from "./components/userAvatar/index";
+
+function App() {
+  return (
+    <section>
+      <h1>aaaa</h1>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/userAvatar/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function userAvatar() {
+  return <div className="userAvatar" />;
+}
+
+export default userAvatar;
