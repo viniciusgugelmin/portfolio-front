6c2069d574137e41c3d695c3c9381dd81126368c
WelingtonMoraes <53630451+WelingtonMoraes@users.noreply.github.com>
2022-01-05 13:41:35 -0300

    add animation avatar user

--- src/components/userAvatar/index.tsx
+++ src/components/userAvatar/index.tsx
@@ -1,13 +1,24 @@
+/* eslint-disable react/style-prop-object */
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
+
+import logo from "./user.png";
+
+const bgAvatar = {
+  color: "white",
+  backgroundImage: `url(${logo})`,
+  fontSize: 200,
+};
 
 export function UserAvatar() {
   return (
-    <div className="userAvatar">
-      <img
-        src={../}
-        alt=""
-      />
-      <h1>aaa</h1>
+    <div className="userAvatar alignVertical">
+      <div className="col-4">
+        <div className="userAvatar__avatar" style={bgAvatar} />
+      </div>
+      <div className="col-8">
+        <h1>nome</h1>
+      </div>
     </div>
   );
 }
