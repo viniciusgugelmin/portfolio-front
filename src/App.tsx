7abc3190f65770eef25a4c2d5ac1620b6c32f5fa
WelingtonMoraes <53630451+WelingtonMoraes@users.noreply.github.com>
2022-05-23 13:17:10 -0300

    Ajuste prettier e ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import "./App.css";
 import { UserAvatar } from "./components/userAvatar/index";
 
 function App(): JSX.Element {
-  return (
-    <div className="container">
-      <div className="container__heightFull alignVertical">
-        <UserAvatar />
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="container__heightFull alignVertical">
+				<UserAvatar />
+				<UserAvatar />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/userAvatar/index.tsx
+++ src/components/userAvatar/index.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { FaFacebook, FaLinkedin, FaGithubSquare } from "react-icons/fa";
 
-import logo from "./vini.jpg";
+import logo from "./user.png";
 
 const bgAvatar = {
-  color: "white",
-  backgroundImage: `url(${logo})`,
-  fontSize: 200,
+	color: "white",
+	backgroundImage: `url(${logo})`,
+	fontSize: 200,
 };
 
 export function UserAvatar() {
-  const url = "";
+	const url = "";
 
-  return (
-    <div className="col-12 alignVertical">
-      <div className="userAvatar boxShadow alignVertical">
-        <div className="col-4">
-          <div className="userAvatar__avatar" style={bgAvatar} />
-        </div>
-        <div className="col-8">
-          <h2>Vinicius Gugelmin</h2>
-          <p>Desenvolvedor Back-end</p>
-          <div className="col-12 userAvatar__avatar--socialRedes">
-            <FaFacebook />
-            <FaLinkedin />
-            <FaGithubSquare />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="col-16 alignVertical">
+			<div className="userAvatar boxShadow alignVertical">
+				<div className="col-12 userAvatar__image">
+					<div className="userAvatar__avatar" style={bgAvatar} />
+				</div>
+				<div className="col-12">
+					<h2>Vinicius Gugelmin</h2>
+					<p>Desenvolvedor Back-end</p>
+					<div className="col-12 userAvatar__avatar--socialRedes">
+						<FaFacebook />
+						<FaLinkedin />
+						<FaGithubSquare />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
