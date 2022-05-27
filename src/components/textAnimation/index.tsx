3ad76a66e650f367370154f23f7d29a3a55df577
Welington Moraes <53630451+WelingtonMoraes@users.noreply.github.com>
2022-05-27 00:46:09 -0300

    portifolio page

---
+++ src/components/textAnimation/index.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { useInterval } from "react-use";
+import styled, { keyframes } from "styled-components";
+
+export function TextAnimation() {
+	const reactArray = "React".split("");
+	const swiftArray = "swift".split("");
+	const [items, setItems] = useState(reactArray);
+	const [count, setCount] = useState(0);
+	const [play, setPlay] = useState(false);
+
+	useInterval(
+		() => {
+			setItems(reactArray);
+			setCount(count + 1);
+
+			// eslint-disable-next-line eqeqeq
+			if (count == 1) {
+				setCount(0);
+				setItems(swiftArray);
+			}
+		},
+		play ? 6000 : null,
+	);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setItems(swiftArray);
+			setPlay(true);
+		}, 4000);
+
+		return () => clearTimeout(timer);
+	}, []);
+
+	return (
+		// eslint-disable-next-line @typescript-eslint/no-use-before-define
+		<Wrapper>
+			{reactArray.map((item, index) => (
+				// eslint-disable-next-line react/no-array-index-key
+				<span key={index}>{item}</span>
+			))}
+		</Wrapper>
+	);
+}
+
+const animation = keyframes`
+    50% {opacity : 1; transform: scale(1) translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg); filter: blur(0px);}
+    60% {opacity : 0; transform: scale(2) translateX(100px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter: blur(10px);}
+    70% {opacity : 0; transform: scale(2) translateY(-100px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter: blur(10px);}
+    80% {opacity : 1; transform: scale(1) translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg); filter: blur(0px);}
+`;
+
+const Wrapper = styled.span`
+	display: inline-block;
+    flex-direction: row;
+    font weight: bold;
+
+	span {
+		animation-name: ${animation};
+		animation-duration: 6s;
+		animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+		animation-fill-mode: forwards;
+		animation-iteration-count: infinite;
+	}
+
+	span:nth-child(1) {
+		animation-delay: 0.1s;
+	}
+	span:nth-child(2) {
+		animation-delay: 0.2s;
+	}
+	span:nth-child(3) {
+		animation-delay: 0.3s;
+	}
+	span:nth-child(4) {
+		animation-delay: 0.4s;
+	}
+	span:nth-child(5) {
+		animation-delay: 0.5s;
+	}
+`;
--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,4 +1,7 @@
+import { ReactTypical } from "@deadcoder0904/react-typical";
 import React from "react";
+
+import Particles from "react-particles-js";
 
 import { NavBar } from "../../components/navBar/index";
 
@@ -8,7 +11,39 @@ export function PortfolioPage() {
 			<NavBar name="Welington Moraes" img="./well.jpeg" />
 
 			<div className="portifolio">
-				<p>text</p>
+				<div className="alignVertical">
+					<div className="col-7">
+						<div className="portifolio__welcome">
+							<p className="portifolio__welcome--subtitle">
+								WELCOME TO MY WORLD
+							</p>
+							<h2 className="portifolio__welcome--title">
+								Hi, I’m <span>Welington</span>
+								<br />a
+								<ReactTypical
+									loop={Infinity}
+									wrapper="b"
+									steps={[
+										" Developer",
+										4000,
+										" Front-End",
+										4000,
+									]}
+								/>
+								.
+							</h2>
+							<p className="portifolio__welcome--description">
+								I use animation as a third dimension by which to
+								simplify experiences and kuiding thro each and
+								every interaction. I’m not adding motion just to
+								spruce things up, but doing it in ways that.
+							</p>
+						</div>
+					</div>
+					<div className="col-5">
+						<Particles />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
